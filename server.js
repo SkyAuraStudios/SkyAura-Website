fdc186cd7e5501a43fbad005b4bc272198ca346d
Cybrix <cybrixtheproot@proton.me>
2025-06-04 03:36:14 -0400

    First Version

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const app = express()
+
+app.set('view engine', 'ejs');
+app.use(express.static('./public/assets'))
+app.set('views', './public/views');
+
+app.get('/', (req, res)=>{
+    res.render('index')
+})
+
+
+app.listen(7528, ()=>{
+    console.log("SkyAura Website version: " + " Is now running on port 7528!")
+})

75cd8c1ba64423e4c06b44401d281a538f8e06b6
Cybrix <cybrixtheproot@proton.me>
2026-01-21 23:26:45 -0500

    Update server.js

--- server.js
+++ server.js
@@ -17,6 +17,6 @@ app.get('/', (req, res) => {
 
 
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');          
+app.listen(7528, () => {
+    console.log('Server is running on port 7528');          
 });
